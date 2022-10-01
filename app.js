b6e96980a939c3f11e11a1365a927de1d1cdd934
MohamedDiini <70833715+MohamedDiini@users.noreply.github.com>
2022-10-01 13:46:34 +0300

    formating template with css and js

--- app.js
+++ app.js
@@ -4,22 +4,29 @@ const path = require('path')
 const port = 3000
 
 
-app.use(express.static('views'))
+// app.use(express.static('views'))
+app.use(express.static(__dirname + '/public'));
 
-app.set('views',path.join(__dirname, '/views'), )
-app.set('view engine','html')
+// app.set('views',path.join(__dirname, '/views'), )
+app.set('view engine','ejs')
 
 
 
 // app.use('/css', express.static(__dirname + '/views/assets/css'));
 
-console.log('_dirname ',path.join(__dirname+'/views/'));
+
 app.get('/',  (req, res) => {
 //   res.send('hello world')
-    // res.render('home')
-    res.sendFile(path.join(__dirname+'/views/login.html'))
+    res.render('login')
+    // res.sendFile(path.join(__dirname+'/views/login.ejs'))
 })
 
+app.get('/home',  (req, res) => {
+    //   res.send('hello world')
+        res.render('index')
+        // res.sendFile(path.join(__dirname+'/views/login.ejs'))
+    })
+    
 
 app.get('/rand', (req,res)=>{
     const num = Math.floor(Math.random()*10)+1;
